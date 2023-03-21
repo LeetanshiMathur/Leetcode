678c385b6b4376435ba2a2bbc2bf2cb7a88d5f56
LeetanshiMathur <90102937+LeetanshiMathur@users.noreply.github.com>
2023-03-21 23:26:02 +0530

    Time: 25 ms (23.68%), Space: 20.4 MB (34.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,7 +4,7 @@ public:
         if(n == 0) return true;
        
         for(int i =0 ;i< flowerbed.size();i++)
-            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) &&( i==flowerbed.size()-1 || flowerbed[i+1]==0                 ))
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) &&(i==flowerbed.size()-1 || flowerbed[i+1] == 0))
                {
                 flowerbed[i] = 1;
                if(--n ==0) return true;
