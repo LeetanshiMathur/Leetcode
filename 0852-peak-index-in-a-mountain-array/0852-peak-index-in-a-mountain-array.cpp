35570b494bea4167f7bde94923b673e5be2b3598
LeetanshiMathur <90102937+LeetanshiMathur@users.noreply.github.com>
2023-08-03 00:03:53 +0530

    Time: 145 ms (5.11%), Space: 62.2 MB (7.44%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -31,26 +31,36 @@ public:
     //         return idx;
     // }
    int peakIndexInMountainArray(vector<int>& arr) {
-    int ans;
-    int idx = -1;
-    int a = INT_MIN; // Set a very small value as the initial maximum
+   //  int ans;
+   //  int idx = -1;
+   //  int a = INT_MIN; // Set a very small value as the initial maximum
 
     // Find the peak element and its index in the original array
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] > a) {
-            a = arr[i];
-            idx = i;
-        }
-    }
+    // for (int i = 0; i < arr.size(); i++) {
+    //     if (arr[i] > a) {
+    //         a = arr[i];
+    //         idx = i;
+    //     }
+    // }
 
     // Check if the peak index is valid and satisfies the conditions of a mountain
-    if (idx > 0 && idx < arr.size() - 1 && arr[idx - 1] < arr[idx] && arr[idx] > arr[idx + 1]) {
-        ans = idx;
-    } else {
-        ans = -1; // Peak index is not valid or does not satisfy mountain conditions
-    }
+    // if (idx > 0 && idx < arr.size() - 1 && arr[idx - 1] < arr[idx] && arr[idx] > arr[idx + 1]) {
+    //     ans = idx;
+    // } else {
+    //     ans = -1; // Peak index is not valid or does not satisfy mountain conditions
+//     }
 
-    return ans;
+//     return ans;
+       vector<int>vect = arr;
+      
+        sort(vect.begin(), vect.end());
+        int a = vect[vect.size()-1];
+        int idx;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]==a)
+                idx = i;
+        }
+       return idx;
 }
 
  };
